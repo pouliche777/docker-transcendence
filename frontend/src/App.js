67605e1d95b3c37c08b6ef147977497f4c1dca7f
Samuel Lord <sam.lord@hotmail.ca>
2023-07-31 11:06:36 -0400

    volumes fonctionnels

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>Hello, React! Un test pour verifier si les poulets volent</h1>
       <p>This is a basic React app.</p>
     </div>
   );
